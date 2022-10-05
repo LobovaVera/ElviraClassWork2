082f9a507c3597fbd430c9e4ab16b8dfdf4b1816
Vera Lobova <pchilli@yandex.ru>
2022-10-05 12:11:48 +0300

    initial commit

---
+++ TASK4/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+
+//12. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. 
+//Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+/*34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно*/
+
+
+
+Console.WriteLine("Enter number one");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter number two");
+int numberTwo = int.Parse(Console.ReadLine()!);
+
+if ( number > numberTwo)
+{
+    int count = 0;
+    while (number >= numberTwo)
+    {
+        number = number - numberTwo;
+        //count++; 
+       
+
+    }
+     if (number == 0)
+        {
+            Console.WriteLine(" Kratno");
+        }
+    else
+    {
+        Console.WriteLine($"Ne kratno, ostatok {number}");
+
+    }
+    }
+else 
+{
+    Console.WriteLine("ERROR");
+}
+
+
+/*Console.WriteLine("Enter number one");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter number two");
+int numberTwo = int.Parse(Console.ReadLine()!);
+
+    int newNumber = number%numberTwo;
+    if (newNumber == 0)
+        {
+            Console.WriteLine(" Kratno");
+        }
+    else
+    {
+        Console.WriteLine($"Ne kratno, ostatok {newNumber}");
+
+    }*/
+
+
+
+   
+
+
+
---
+++ TASK6/Program.cs
@@ -0,0 +1,37 @@
+﻿//16. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+/*5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет*/
+
+Console.WriteLine("Enter number one");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter number two");
+int numberTwo = int.Parse(Console.ReadLine()!);
+
+if ( number> numberTwo)
+{
+    if(number == numberTwo*numberTwo)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+
+    }
+}
+else
+{
+    if(numberTwo == number*number)
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+
+    }
+
+}
